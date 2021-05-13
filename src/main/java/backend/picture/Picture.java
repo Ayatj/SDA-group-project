9fcee8fa5db9e0600cb0a2b2046e01bc1d4fcc52
Ayatj <79480683+Ayatj@users.noreply.github.com>
2021-05-13 15:29:56 +0200

    backup but not complete

---
+++ src/main/java/backend/picture/Picture.java
@@ -0,0 +1,70 @@
+/*
+package backend.avatar;
+
+import backend.user.User;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+
+@Entity
+@Table
+public class Avatar {
+    //fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column
+    private long id;
+
+    public Avatar(long id) {
+        this.id = id;
+    }
+
+     */
+/*@NotBlank
+    @Column(nullable = false)
+    private String name;
+*//*
+
+
+
+    */
+/*@OneToOne(targetEntity = User.class, cascade = CascadeType.ALL)
+    private User avatarowner;*//*
+
+
+  */
+/*  public User getAvatarowner() {
+        return avatarowner;
+    }
+
+    public void setAvatarowner(User avatarowner) {
+        this.avatarowner = avatarowner;
+    }*//*
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+ */
+/*   public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }*//*
+
+
+
+
+    public Avatar() {
+    }
+
+}
+*/
